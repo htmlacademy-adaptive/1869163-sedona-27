da7e84480e6ee73b5df94a5fe4cadb1072ccdf62
vhs-cake <vali.kravtsova@gmail.com>
2023-01-15 12:04:01 +0100

    Пиксель перфект для индекса и каталога

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,8 @@ let navMain = document.querySelector(".nav");
 let navToggle = document.querySelector(".nav__toggle");
 
 navMain.classList.remove(".nav--nojs");
+navMain.classList.add("nav--closed");
+navMain.classList.remove("nav--opened");
 
 navToggle.addEventListener("click", function () {
   if (navMain.classList.contains("nav--closed")) {
