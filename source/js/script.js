40f7852620d43be22e2fc8a4bf1ea87a30ca9139
vhs-cake <vali.kravtsova@gmail.com>
2023-01-21 10:54:07 +0100

    Пиксель перфект всех страниц

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,13 @@
-let navMain = document.querySelector(".nav");
-let navToggle = document.querySelector(".nav__toggle");
+const navMain = document.querySelector(".nav");
+const navToggle = document.querySelector(".nav__toggle");
+const header = document.querySelector(".header");
+const map = document.querySelector(".map");
 
-navMain.classList.remove(".nav--nojs");
+navMain.classList.remove("nav--nojs");
 navMain.classList.add("nav--closed");
 navMain.classList.remove("nav--opened");
+header.classList.remove("header--nojs");
+map.classList.remove("map--nojs");
 
 navToggle.addEventListener("click", function () {
   if (navMain.classList.contains("nav--closed")) {
